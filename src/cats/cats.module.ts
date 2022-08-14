6791609544e6a4413494f9f7f1380cce1f5dc1c8
Erik Ciau <erikciau777@icloud.com>
2022-08-13 22:06:38 -0500

    nest middlewares: learning nest middlewares

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -1,10 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { CatsController } from './cats.controller';
 import { IndentifierModule } from '../indentifier/indentifier.module'
+import { LoggerMiddleware } from './cats.middleware'
 
 @Module({
   controllers: [CatsController],
   // 👀 Se injecta el modulo, no el servicio a usar
   imports: [IndentifierModule]
 })
-export class CatsModule { }
+export class CatsModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes(CatsController)
+  }
+}
